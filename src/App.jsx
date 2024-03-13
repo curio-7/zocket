925cbbff33f7888511499c15377425a3a4135fbe
Sarthak Nigam <srthkngm@gmail.com>
2024-03-13 16:39:33 +0530

    Refactor App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import './App.css'
 import Box from './components/Edit/Box'
 import Display from './components/Canva/Display'
-import { Provider } from 'react-redux'
-import {store} from './app/store.js'
 
 function App() {
 
   return (
-    <Provider store={store}>
-      <div className='flex flex-col sm:flex-row w-screen'>
-    
-      <Display/>
-      <Box/>
+    <div className='flex flex-col sm:flex-row w-screen'>
+  
+    <Display/>
+    <Box/>
 
-      </div>
-    </Provider>
+    </div>
   )
 }
 
